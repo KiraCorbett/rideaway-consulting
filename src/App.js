9c721442ef0b1830bd701c98f2a4784bec635d23
Kira Corbett <kiracorbett00@gmail.com>
2023-09-15 15:45:07 -0700

    init container layout

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
-import logo from './logo.svg';
+import logo from './images/dark-logo.png';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
+    <div>
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+          Cascades Intelligence Associates
+          </p>
+        </header>
+      </div>
+      <div className="Container">
+        <p>Text</p>
+        <p>Text</p>
+        <p>Text</p>
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      </div>
     </div>
   );
 }
